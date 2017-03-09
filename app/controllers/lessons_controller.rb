b7b8cda46c4f9833f8cf022385d99e998fdaf7ce
Tyler Braddock <tyler.braddock@yahoo.com>
2017-03-09 20:49:11 +0000

    In controllers/lessons_controller.rb, added before_action to authenticate user. Added before_action require_enrollment_for_lesson_access with private method by same name, for preventing access to lessons unless enrolled. Added private method current_course.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,7 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrollment_for_lesson_access
+
   def show
   end
 
@@ -7,5 +10,15 @@ class LessonsController < ApplicationController
   helper_method :current_lesson
   def current_lesson
     @current_lesson ||= Lesson.find(params[:id])
+  end
+
+  def current_course
+    @current_course ||= current_lesson.section.course
+  end
+
+  def require_enrollment_for_lesson_access
+    if current_course.user != current_user && !current_user.enrolled_in?(current_course)
+      redirect_to course_path(current_course), alert: 'You must be enrolled to view this lesson.'
+    end
   end
 end
